2ffea804c8ab98f58fb3388d4ec6af79b0ffa894
Lartg <roc.gerrety@gmail.com>
2023-07-04 15:21:13 -0700

    added a lot more comments, added todos, no progress on the readme

--- Tutorial/bot.go
+++ Tutorial/bot.go
@@ -26,8 +26,8 @@ func main() {
 	}
 
 	// Register messageCreate handler
-	dg.AddHandler(messageCreate)
-
+	dg.AddHandler(diceCreate)
+	dg.AddHandler(webScraperCreate)
 	// Open a websocket connection to Discord.
 	err = dg.Open()
 	if err != nil {
@@ -40,40 +40,83 @@ func main() {
 	<-make(chan struct{})
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore messages from the bot itself or other bots
+/*	----------------------------------------------------------------------------
+	Check if the message starts with "!"
+		Get the following message string that === dice roll (e.g. 1d6, 2d6, 1d8)
+			log message and respose
+			Send a response message containing dice rolls.
+*/
+
+func diceCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore messages from the bot itself or other bots to prevent recursive requests
 	if m.Author.ID == s.State.User.ID || m.Author.Bot {
 		return
 	}
 
-	// Check if the message starts with "./"
+	// Parse command
+	if strings.HasPrefix(m.Content, "!") {
+		fmt.Println("we have not yet coded dice rolls.")
+	}
+}
+
+/* ----------------------------------------------------------------------------
+Check if the message starts with "./"
+	Get the following message string.
+		log message and resonse
+		Send a response message.
+
+*/
+
+func webScraperCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore messages from the bot itself or other bots to prevent recursive requests
+	if m.Author.ID == s.State.User.ID || m.Author.Bot {
+		return
+	}
+
+	// Parse command
 	if strings.HasPrefix(m.Content, "./") {
 		query := strings.TrimPrefix(m.Content, "./")
 		baseURL := "https://www.formula1.com"
 
-		// Create a new collector
+		// Create a new collector to scrape our baseURL
 		c := colly.NewCollector()
 
 		//--------------------------------------------------------
-		// enter css selector for the article card
+		/*
+			This function isolates the HTML elements with the css selectors matching the first parameter.
+			Then we do cool things and send a response to our discord server messaging channel
+
+			TODO:
+			add the response as a thread to the request, and have a reply for no matches
+		*/
+
 		c.OnHTML("#article-list .col-12", func(e *colly.HTMLElement) {
+
 			// get article card title
-			// if title contains query store anchor
 			title := e.ChildTexts("p")
+
+			// if title contains query store anchor
 			queryCheck := strings.Contains((title[1]), query)
 			if queryCheck {
+				// get the anchor for the article
 				anchor := e.ChildAttr("a", "href")
+
+				// make a link to the article by adding the anchor to our base URL
 				newURL := baseURL + anchor
+
+				// send link to matched article
 				s.ChannelMessageSend(m.ChannelID, newURL)
 			}
 		})
 
 		//--------------------------------------------------------
-		// Visit the search URL and scrape the page
+		// Visit the search URL and scrape the page with our callback
 		err := c.Visit(baseURL + "/en/latest/all.html")
 		if err != nil {
 			log.Println("Error scraping Google search page: ", err)
 			return
 		}
+		fmt.Println("Successfully scraped f1 latest news, if no article sent there may be no matches, will create a message response for this case")
+
 	}
 }
