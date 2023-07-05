22932da9c048206c2db1943941ff0fe7177a58e8
Lartg <roc.gerrety@gmail.com>
2023-07-05 16:45:56 -0700

    renamed

---
+++ 02-Create-and-Connect-to-a-New-Discord-Bot/bot.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	// Create a new Discord session.
+	dg, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
+	if err != nil {
+		log.Fatal("Error creating Discord session: ", err)
+		return
+	}
+
+	// Open a websocket connection to Discord.
+	err = dg.Open()
+	if err != nil {
+		log.Fatal("Error opening connection: ", err)
+		return
+	}
+
+	// Wait here until interrupted.
+	fmt.Println("Bot is now running. Press Ctrl+C to exit.")
+	<-make(chan struct{})
+}
