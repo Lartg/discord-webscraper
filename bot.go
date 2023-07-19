78cbcf97933186f99e093c9e01121e650c777fce
Lartg <roc.gerrety@gmail.com>
2023-07-18 18:08:59 -0800

    organized command-library to avoid more techinical debt

--- bot.go
+++ bot.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
+	"github.com/Lartg/discord-webscraper/command-library/dice"
+	"github.com/Lartg/discord-webscraper/command-library/f1Scraper"
 	"github.com/bwmarrin/discordgo"
-	"github.com/gocolly/colly/v2"
 	"github.com/joho/godotenv"
 )
 
@@ -26,8 +26,9 @@ func main() {
 	}
 
 	// Register command handlers - these read messages for prefixes to check if they need to do things. commands could get very complex using many flags
-	dg.AddHandler(webScraperCreate) // uses "./" message prefix
-	dice.roll()
+	dg.AddHandler(f1Scraper.Scrape) // uses "./" message prefix
+	dg.AddHandler(dice.Roll)        // uses "!" message prefix
+
 	// Open a websocket connection to Discord.
 	err = dg.Open()
 	if err != nil {
@@ -38,66 +39,4 @@ func main() {
 	// Wait here until interrupted.
 	fmt.Println("Bot is now running. Press Ctrl+C to exit.")
 	<-make(chan struct{})
-}
-
-/* ----------------------------------------------------------------------------
-This Function will:
-	Check if the message starts with "./"
-		Get the command string.
-		Scrape a website for matches to the command
-			return links to matches
-*/
-
-func webScraperCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore messages from the bot itself or other bots to prevent recursive requests
-	if m.Author.ID == s.State.User.ID || m.Author.Bot {
-		return
-	}
-
-	// Parse command
-	if strings.HasPrefix(m.Content, "./") {
-		query := strings.TrimPrefix(m.Content, "./")
-		baseURL := "https://www.formula1.com"
-
-		// Create a new collector to scrape our baseURL
-		c := colly.NewCollector()
-
-		//--------------------------------------------------------
-		/*
-			This function isolates the HTML elements with the css selectors matching the first parameter.
-			Then we do cool things and send a response to our discord server messaging channel
-
-			TODO:
-			add the response as a thread to the request, and have a reply for no matches
-		*/
-
-		c.OnHTML("#article-list .col-12", func(e *colly.HTMLElement) {
-
-			// get article card title
-			title := e.ChildTexts("p")
-
-			// if title contains query store anchor
-			queryCheck := strings.Contains((title[1]), query)
-			if queryCheck {
-				// get the anchor for the article
-				anchor := e.ChildAttr("a", "href")
-
-				// make a link to the article by adding the anchor to our base URL
-				newURL := baseURL + anchor
-
-				// send link to matched article
-				s.ChannelMessageSend(m.ChannelID, newURL)
-			}
-		})
-
-		//--------------------------------------------------------
-		// Visit the search URL and scrape the page with our callback
-		err := c.Visit(baseURL + "/en/latest/all.html")
-		if err != nil {
-			log.Println("Error scraping Google search page: ", err)
-			return
-		}
-		fmt.Println("Successfully scraped f1 latest news, if no article sent there may be no matches, will create a message response for this case")
-
-	}
 }
--- command-library/dice/dice.go
+++ command-library/dice/dice.go
@@ -1,7 +1,69 @@
 package dice
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
 
-func roll() {
-	fmt.Println("hello world")
+	"github.com/bwmarrin/discordgo"
+)
+
+/*	----------------------------------------------------------------------------\
+This Function will:
+	Check if the message starts with "!"
+		Get the following message string that corresponds to a dice roll (e.g. 1d6, 2d6, 1d8)
+			Send a response message containing roll outcomes.
+*/
+
+func Roll(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore messages from the bot itself or other bots to prevent recursive requests
+	if m.Author.ID == s.State.User.ID || m.Author.Bot {
+		return
+	}
+
+	// --------------------NEW STUFF BELOW-----------------------
+	if strings.HasPrefix(m.Content, "!") {
+		command := strings.TrimPrefix(m.Content, "!")
+		result := rollDice(command)
+		if result != nil {
+			response := fmt.Sprintf("Rolling %s: ", command)
+			for i, roll := range result {
+				if i > 0 {
+					response += ", "
+				}
+				response += fmt.Sprintf("%d", roll)
+			}
+			s.ChannelMessageSend(m.ChannelID, response)
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "Invalid dice roll command. Use form of How many dice rolled, a 'd' for dice, and how many sides. Ex. 1d6 rolls 1 dice with 6 sides.")
+		}
+	}
+}
+
+// Our Dice rolling helper function
+func rollDice(roll string) []int {
+	parts := strings.Split(roll, "d")
+	if len(parts) != 2 {
+		return nil
+	}
+
+	numDice, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return nil
+	}
+	numSides, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return nil
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	results := make([]int, numDice)
+	for i := 0; i < numDice; i++ {
+		results[i] = rand.Intn(numSides) + 1
+	}
+
+	return results
 }
